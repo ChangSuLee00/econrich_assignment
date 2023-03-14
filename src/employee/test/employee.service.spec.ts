ca803fc4fadbd7fb060c9f667eb16b0bdeb82c5a
cslee <cslee0034@gmail.com>
2023-03-14 17:43:10 +0900

    test: Add employee service findOne
    employee service의 findOne 테스트를 추가 했습니다.

--- src/employee/test/employee.service.spec.ts
+++ src/employee/test/employee.service.spec.ts
@@ -1,18 +1,59 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeService } from '../employee.service';
+import { EmployeeRepository } from '../repository/employee.repository';
+
+const mockEmployeeRepository = () => ({
+  findOne: jest.fn(),
+});
 
 describe('EmployeeService', () => {
-  let service: EmployeeService;
+  let spyEmployeeService: EmployeeService;
+  let spyEmployeeRepository: EmployeeRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmployeeService],
+      providers: [
+        EmployeeService,
+        { provide: EmployeeRepository, useFactory: mockEmployeeRepository },
+      ],
     }).compile();
 
-    service = module.get<EmployeeService>(EmployeeService);
+    spyEmployeeService = module.get<EmployeeService>(EmployeeService);
+    spyEmployeeRepository = module.get<EmployeeRepository>(EmployeeRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('FindEmployee', () => {
+    it('직원 찾기 성공', async () => {
+      const id = 100;
+      // Method Mocking
+      (spyEmployeeRepository.findOne as jest.Mock).mockReturnValue({
+        employee_id: 100,
+      });
+      // Excuute
+      const result = await spyEmployeeService.findOne(id);
+      // Expect
+      expect(spyEmployeeRepository.findOne).toBeCalled();
+      expect(spyEmployeeRepository.findOne).toBeCalledWith(id);
+      expect(result).toEqual({
+        employee_id: 100,
+      });
+    });
+
+    it('직원 찾기 실패', async () => {
+      const id = 99;
+      // Method Mocking
+      (spyEmployeeRepository.findOne as jest.Mock).mockRejectedValue(
+        new NotFoundException(),
+      );
+      try {
+        // Excuute
+        const result = await spyEmployeeService.findOne(id);
+      } catch (error) {
+        // Expect
+        expect(error).toBeTruthy;
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
