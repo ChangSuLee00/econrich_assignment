609d43a10b1e8e4bc0f67470371f853a91ae8520
cslee <cslee0034@gmail.com>
2023-03-14 17:25:05 +0900

    feat: write employee service
    employee의 service를 작성 했습니다.

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateEmployeeDto } from './dto/create-employee.dto';
-import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { EmployeeRepository } from './repository/employee.repository';
 
 @Injectable()
 export class EmployeeService {
-  create(createEmployeeDto: CreateEmployeeDto) {
-    return 'This action adds a new employee';
-  }
+  constructor(private readonly employeeRepository: EmployeeRepository) {}
 
-  findAll() {
-    return `This action returns all employee`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} employee`;
-  }
-
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
-    return `This action updates a #${id} employee`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} employee`;
+  async findOne(id: number) {
+    const employee = await this.employeeRepository.findOne(id);
+    return employee;
   }
 }
