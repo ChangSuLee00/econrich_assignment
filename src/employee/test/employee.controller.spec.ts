519b1fa37aa42a3a28adbc775bedbede3c2c766f
cslee <cslee0034@gmail.com>
2023-03-14 17:29:03 +0900

    test: Add test employee controller findOne
    employee controller의 findOne의 테스트를 추가 했습니다.

--- src/employee/test/employee.controller.spec.ts
+++ src/employee/test/employee.controller.spec.ts
@@ -1,20 +1,57 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeController } from '../employee.controller';
 import { EmployeeService } from '../employee.service';
 
+const mockEmployeeService = () => ({
+  findOne: jest.fn((id) => {
+    if (id === 100) {
+      return [{ employee_id: 100 }];
+    } else {
+      return NotFoundException;
+    }
+  }),
+});
+
 describe('EmployeeController', () => {
   let controller: EmployeeController;
+  let spyEmployeeService: EmployeeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EmployeeController],
-      providers: [EmployeeService],
+      providers: [
+        { provide: EmployeeService, useFactory: mockEmployeeService },
+      ],
     }).compile();
 
     controller = module.get<EmployeeController>(EmployeeController);
+    spyEmployeeService = module.get<EmployeeService>(EmployeeService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('FindEmployee', () => {
+    it('직원 찾기 성공', async () => {
+      const id = 100;
+
+      // Excute
+      const response = await controller.findOne(id);
+
+      // Expect
+      expect(spyEmployeeService.findOne).toBeCalled();
+      expect(spyEmployeeService.findOne).toBeCalledWith(id);
+      expect(response).toEqual([{ employee_id: 100 }]);
+    });
+
+    it('직원 찾기 실패', async () => {
+      const id = 99;
+
+      // Excute
+      const response = await controller.findOne(id);
+
+      // Expect
+      expect(spyEmployeeService.findOne).toBeCalled();
+      expect(spyEmployeeService.findOne).toBeCalledWith(id);
+      expect(response).toEqual(NotFoundException);
+    });
   });
 });
