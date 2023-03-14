0e2cf60cfd4f0d56118f4acb9e4506dfa4f169d2
cslee <cslee0034@gmail.com>
2023-03-14 17:25:32 +0900

    feat: Import employee repository to module
    module에 repository를 추가 했습니다.

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EmployeeRepository } from './repository/employee.repository';
+import ormconfig from 'src/ormconfig';
 
 @Module({
+  imports: [TypeOrmModule.forRoot(ormconfig)],
   controllers: [EmployeeController],
-  providers: [EmployeeService]
+  providers: [EmployeeService, EmployeeRepository],
 })
 export class EmployeeModule {}
