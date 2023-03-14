466342a5cad10412086864576827bf0bb162cb7f
cslee <cslee0034@gmail.com>
2023-03-14 17:10:18 +0900

    feat: Add logger, exception filter
    winston logger와 exception filter를 추가 했습니다.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { AllExceptionFiller } from './common/exception/exception.filter';
+import { winstonLogger } from './common/utils/logger.winston';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: winstonLogger,
+  });
+  app.useGlobalFilters(new AllExceptionFiller(winstonLogger));
   await app.listen(5000);
 }
 bootstrap();
