31ce0e8339f95a3930a5125cda461eb97aff39b8
cslee <cslee0034@gmail.com>
2023-03-17 21:35:36 +0900

    feat: Add repository air API
    시 도 별 공기질 측정 API의 repository를 작성 했습니다.

--- src/air/repository/employee.repository.ts
+++ src/air/repository/employee.repository.ts
@@ -1,13 +1,19 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { SidoName } from '../interfaces/sidoName.type';
+import axios from 'axios';
 
 @Injectable()
 export class AirRepository {
   constructor(private readonly connection: DataSource) {}
 
   async findAirBySidoName(query: SidoName): Promise<any> {
+    const serviceKey = process.env.serviceKey;
+    const sidoName = encodeURI(query.sidoName);
+    const url = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=${sidoName}&pageNo=1&numOfRows=100&returnType=json&serviceKey=${serviceKey}`;
     try {
+      const response = await axios.get(url);
+      return response.data.response.body.items;
     } catch (error) {
       throw new InternalServerErrorException('error while find air condition');
       // 페이지 또는 파일을 찾을 수 없음 404
