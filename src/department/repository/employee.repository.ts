cea15b1bbf69d3481e897417db61781a02318b49
cslee <cslee0034@gmail.com>
2023-03-17 18:55:20 +0900

    feat: Add repository department findLocation
    department의 findLocation의 repository를 작성 했습니다.

--- src/department/repository/employee.repository.ts
+++ src/department/repository/employee.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { DepartmentFindOne } from '../interfaces/findDept.type';
+import { DepartmentFindLocation } from '../interfaces/findDeptLocation.type';
 
 @Injectable()
 export class DepartmentRepository {
@@ -13,6 +14,21 @@ export class DepartmentRepository {
       return result;
     } catch (error) {
       throw new NotFoundException('error while find department');
+      // 페이지 또는 파일을 찾을 수 없음 404
+    }
+  }
+
+  async findLocation(id: number): Promise<DepartmentFindLocation> {
+    try {
+      const query = `SELECT locations.*
+      FROM departments
+      JOIN locations 
+      ON departments.location_id = locations.location_id
+      WHERE departments.department_id = ${id}`;
+      const result = await this.connection.query(query);
+      return result;
+    } catch (error) {
+      throw new NotFoundException('error while find department location');
       // 페이지 또는 파일을 찾을 수 없음 404
     }
   }
