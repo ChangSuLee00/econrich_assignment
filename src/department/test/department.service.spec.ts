4e45930fda587c6ba01d84ab524cc96d9060ce15
cslee <cslee0034@gmail.com>
2023-03-17 18:41:13 +0900

    test: Add test department service findOne
    department service의 findOne의 test를 추가 했습니다.

--- src/department/test/department.service.spec.ts
+++ src/department/test/department.service.spec.ts
@@ -1,18 +1,60 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DepartmentService } from '../department.service';
+import { DepartmentRepository } from '../repository/employee.repository';
+
+const mockDepartmentRepository = () => ({
+  findOne: jest.fn(),
+});
 
 describe('DepartmentService', () => {
-  let service: DepartmentService;
+  let spyDepartmentService: DepartmentService;
+  let spyDepartmentRepository: DepartmentRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DepartmentService],
+      providers: [
+        DepartmentService,
+        { provide: DepartmentRepository, useFactory: mockDepartmentRepository },
+      ],
     }).compile();
 
-    service = module.get<DepartmentService>(DepartmentService);
+    spyDepartmentService = module.get<DepartmentService>(DepartmentService);
+    spyDepartmentRepository =
+      module.get<DepartmentRepository>(DepartmentRepository);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('FindEmployee', () => {
+    it('직원 찾기 성공', async () => {
+      const id = 100;
+      // Method Mocking
+      (spyDepartmentRepository.findOne as jest.Mock).mockReturnValue({
+        employee_id: 100,
+      });
+      // Excuute
+      const result = await spyDepartmentService.findOne(id);
+      // Expect
+      expect(spyDepartmentRepository.findOne).toBeCalled();
+      expect(spyDepartmentRepository.findOne).toBeCalledWith(id);
+      expect(result).toEqual({
+        employee_id: 100,
+      });
+    });
+
+    it('직원 찾기 실패', async () => {
+      const id = 99;
+      // Method Mocking
+      (spyDepartmentRepository.findOne as jest.Mock).mockRejectedValue(
+        new NotFoundException(),
+      );
+      try {
+        // Excuute
+        const result = await spyDepartmentService.findOne(id);
+      } catch (error) {
+        // Expect
+        expect(error).toBeTruthy;
+        expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
