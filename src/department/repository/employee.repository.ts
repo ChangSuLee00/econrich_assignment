acaafc05ca900c2212095971c35d9c5389cb6de5
cslee <cslee0034@gmail.com>
2023-03-17 18:31:15 +0900

    feat: Add repository department findOne
    department findOne의 repository를 작성 했습니다.

--- src/department/repository/employee.repository.ts
+++ src/department/repository/employee.repository.ts
@@ -1,12 +1,16 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { DepartmentFindOne } from '../interfaces/findDept.type';
 
 @Injectable()
 export class DepartmentRepository {
   constructor(private readonly connection: DataSource) {}
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<DepartmentFindOne> {
     try {
+      const query = `SELECT * FROM departments WHERE department_id = ${id}`;
+      const result = await this.connection.query(query);
+      return result;
     } catch (error) {
       throw new NotFoundException('error while find department');
       // 페이지 또는 파일을 찾을 수 없음 404
