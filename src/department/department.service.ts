0fedaabc822c7c5fc9d2fd3cf0bf4c5e6f85af12
cslee <cslee0034@gmail.com>
2023-03-17 19:28:07 +0900

    feat: Add department service updateSalary
    department service의 updateSalary를 구현 했습니다.

--- src/department/department.service.ts
+++ src/department/department.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { DepartmentUpdateSalary } from './interfaces/updateDeptSalary.type';
 import { DepartmentRepository } from './repository/employee.repository';
 
 @Injectable()
@@ -13,5 +14,14 @@ export class DepartmentService {
   async findLocation(id: number) {
     const location = await this.departmentRepository.findLocation(id);
     return location;
+  }
+
+  async updateSalary(id: number, body: DepartmentUpdateSalary) {
+    const location = await this.departmentRepository.updateSalary(id, body);
+    if (location) {
+      return { status: 200, success: true };
+    }
+    return { status: 422, success: false };
+    // 요청 문법은 맞지만 지시에 따를 수 없음
   }
 }
