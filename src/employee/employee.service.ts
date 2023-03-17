912286800ca84933089656cf449e5ff24a4fa9cc
cslee <cslee0034@gmail.com>
2023-03-17 16:46:27 +0900

    feat: Add service employee updateOne
    employee updateOne의 service를 작성 했습니다.

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { EmployeeUpdateOne } from './interfaces/updateOne.type';
 import { EmployeeRepository } from './repository/employee.repository';
 
 @Injectable()
@@ -13,5 +14,14 @@ export class EmployeeService {
   async findOneHistory(id: number) {
     const employeeHistory = await this.employeeRepository.findOneHistory(id);
     return employeeHistory;
+  }
+
+  async updateOne(id: number, body: EmployeeUpdateOne) {
+    const updatedEmployee = await this.employeeRepository.updateOne(id, body);
+    if (updatedEmployee) {
+      return { status: 200, success: true };
+    }
+    return { status: 422, success: false };
+    // 요청 문법은 맞지만 지시에 따를 수 없음
   }
 }
