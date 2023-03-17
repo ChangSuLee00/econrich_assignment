503fa72c4ac83ab05845d911c7d9c419d3cd03ff
cslee <cslee0034@gmail.com>
2023-03-17 16:45:36 +0900

    feat: Add controller employee updateOne
    employee updateOne의 controller를 작성 했습니다.

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+} from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeFindHistory } from './interfaces/findHistory.type';
 import { EmployeeFindOne } from './interfaces/findOne.type';
+import { EmployeeUpdateOne } from './interfaces/updateOne.type';
 
 @Controller('employee')
 export class EmployeeController {
@@ -17,5 +25,13 @@ export class EmployeeController {
     @Param('id', ParseIntPipe) id: number,
   ): Promise<EmployeeFindHistory> {
     return this.employeeService.findOneHistory(id);
+  }
+
+  @Patch(':id')
+  updateOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: EmployeeUpdateOne,
+  ): Promise<{ status: number; success: boolean }> {
+    return this.employeeService.updateOne(id, body);
   }
 }
