14373cb835e68c6a1615f0eafc802903e624105c
cslee <cslee0034@gmail.com>
2023-03-17 16:46:58 +0900

    feat: Add repository employee updateOne
    employee updateOne의 repository를 작성 했습니다.

--- src/employee/repository/employee.repository.ts
+++ src/employee/repository/employee.repository.ts
@@ -1,7 +1,12 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { EmployeeFindHistory } from '../interfaces/findHistory.type';
 import { EmployeeFindOne } from '../interfaces/findOne.type';
+import { EmployeeUpdateOne } from '../interfaces/updateOne.type';
 
 @Injectable()
 export class EmployeeRepository {
@@ -26,6 +31,29 @@ export class EmployeeRepository {
     } catch (error) {
       throw new NotFoundException('error while find employee');
       // 페이지 또는 파일을 찾을 수 없음 404
+    }
+  }
+
+  async updateOne(
+    id: number,
+    body: EmployeeUpdateOne,
+  ): Promise<{ status: number; success: boolean }> {
+    // body에 있는 요소를 query 문으로 변환
+    const setQuerys = Object.keys(body).map((key) => {
+      if (body[key] !== undefined) {
+        return `${key} = '${body[key]}'`;
+      }
+    });
+    const setQuery = setQuerys.join(', ');
+
+    // SQL query
+    try {
+      const query = `UPDATE employees SET ${setQuery} WHERE employee_id = ${id}`;
+      const result = await this.connection.query(query);
+      return result;
+    } catch (error) {
+      throw new InternalServerErrorException('error while saving box');
+      // 내부 서버 에러 500
     }
   }
 }
