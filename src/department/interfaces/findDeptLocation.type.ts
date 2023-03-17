2f116eea6cdd20804c852b43f58b5ba9835cd849
cslee <cslee0034@gmail.com>
2023-03-17 18:53:38 +0900

    feat: Add type findDeptLocation
    findDeptLocation의 type을 지정 했습니다.

---
+++ src/department/interfaces/findDeptLocation.type.ts
@@ -0,0 +1,8 @@
+export interface DepartmentFindLocation {
+  location_id: number;
+  street_address: string;
+  postal_code: number;
+  city: string;
+  state_province: string;
+  country_id: string;
+}
