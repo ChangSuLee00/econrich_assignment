33d97ab52be30d319812bb088de38202bb23ac8c
cslee <cslee0034@gmail.com>
2023-03-17 17:09:09 +0900

    test: Add employee controller test updateOne
    employee controller에 updateOne의 test를 추가 했습니다.
    findOneHistory의 test를 수정 했습니다.

--- src/employee/test/employee.service.spec.ts
+++ src/employee/test/employee.service.spec.ts
@@ -1,10 +1,15 @@
-import { NotFoundException } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeService } from '../employee.service';
 import { EmployeeRepository } from '../repository/employee.repository';
 
 const mockEmployeeRepository = () => ({
   findOne: jest.fn(),
+  findOneHistory: jest.fn(),
+  updateOne: jest.fn(),
 });
 
 describe('EmployeeService', () => {
@@ -61,14 +66,14 @@ describe('EmployeeService', () => {
     it('직원 이력 찾기 성공', async () => {
       const id = 101;
       // Method Mocking
-      (spyEmployeeRepository.findOne as jest.Mock).mockReturnValue({
+      (spyEmployeeRepository.findOneHistory as jest.Mock).mockReturnValue({
         employee_id: 101,
       });
       // Excuute
-      const result = await spyEmployeeService.findOne(id);
+      const result = await spyEmployeeService.findOneHistory(id);
       // Expect
-      expect(spyEmployeeRepository.findOne).toBeCalled();
-      expect(spyEmployeeRepository.findOne).toBeCalledWith(id);
+      expect(spyEmployeeRepository.findOneHistory).toBeCalled();
+      expect(spyEmployeeRepository.findOneHistory).toBeCalledWith(id);
       expect(result).toEqual({
         employee_id: 101,
       });
@@ -77,16 +82,68 @@ describe('EmployeeService', () => {
     it('직원 이력 찾기 실패', async () => {
       const id = 100;
       // Method Mocking
-      (spyEmployeeRepository.findOne as jest.Mock).mockRejectedValue(
+      (spyEmployeeRepository.findOneHistory as jest.Mock).mockRejectedValue(
         new NotFoundException(),
       );
       try {
         // Excuute
-        const result = await spyEmployeeService.findOne(id);
+        const result = await spyEmployeeService.findOneHistory(id);
       } catch (error) {
         // Expect
         expect(error).toBeTruthy;
         expect(error).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('UpdateEmployee', () => {
+    it('직원 업데이트 성공', async () => {
+      const id = 101;
+      const body = { first_name: 'Steven' };
+      // Method Mocking
+      (spyEmployeeRepository.updateOne as jest.Mock).mockReturnValue({
+        status: 200,
+        success: true,
+      });
+      // Excuute
+      const result = await spyEmployeeService.updateOne(id, body);
+      // Expect
+      expect(spyEmployeeRepository.updateOne).toBeCalled();
+      expect(spyEmployeeRepository.updateOne).toBeCalledWith(id, body);
+      expect(result).toEqual({ status: 200, success: true });
+    });
+
+    it('직원 업데이트 실패(해당 아이디 없음)', async () => {
+      const id = 99;
+      const body = { first_name: 'Steven' };
+      // Method Mocking
+      (spyEmployeeRepository.updateOne as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException(),
+      );
+      try {
+        // Excuute
+        const result = await spyEmployeeService.updateOne(id, body);
+      } catch (error) {
+        // Expect
+        expect(error).toBeTruthy;
+        expect(error).toBeInstanceOf(InternalServerErrorException);
+      }
+    });
+
+    it('직원 업데이트 실패(body 내용 없음)', async () => {
+      const id = 99;
+      const body = { first_name: 'Steven' };
+      // Method Mocking
+      (spyEmployeeRepository.findOne as jest.Mock).mockRejectedValue(
+        new InternalServerErrorException(),
+      );
+      try {
+        // Excuute
+        const result = await spyEmployeeService.updateOne(id, body);
+      } catch (error) {
+        // Expect
+        expect(error).toBeTruthy;
+        expect(error).toBeInstanceOf(InternalServerErrorException);
       }
     });
   });
