f69156b7660c74d5ccd6fc4cf7de79034e1a1e1d
cslee <cslee0034@gmail.com>
2023-03-17 18:54:09 +0900

    feat: Add controller department findLocation
    department의 findLocation의 controller를 작성 했습니다.

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { DepartmentService } from './department.service';
 import { DepartmentFindOne } from './interfaces/findDept.type';
+import { DepartmentFindLocation } from './interfaces/findDeptLocation.type';
 
 @Controller('department')
 export class DepartmentController {
@@ -9,5 +10,12 @@ export class DepartmentController {
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number): Promise<DepartmentFindOne> {
     return this.departmentService.findOne(id);
+  }
+
+  @Get('location/:id')
+  findLocation(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<DepartmentFindLocation> {
+    return this.departmentService.findLocation(id);
   }
 }
