9b6ede8367ecd192475013c6d93d8b08b8c7a248
cslee <cslee0034@gmail.com>
2023-03-17 23:07:11 +0900

    test: Add e2e test
    e2e 테스트를 추가 했습니다.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,58 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  describe('Employee (e2e)', () => {
+    it('/employee/:id (GET)', () => {
+      return request(app.getHttpServer()).get('/employee/100').expect(200);
+    });
+
+    it('/employee/history/:id (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/employee/history/100')
+        .expect(200);
+    });
+
+    it('/employee/:id (PATCH)', () => {
+      const data = { first_name: 'Stephan' };
+      return request(app.getHttpServer())
+        .patch('/employee/100')
+        .send(data)
+        .expect(200);
+    });
+  });
+
+  describe('Department (e2e)', () => {
+    it('/department/:id (GET)', () => {
+      return request(app.getHttpServer()).get('/department/10').expect(200);
+    });
+
+    it('/department/location/:id (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/department/location/10')
+        .expect(200);
+    });
+
+    it('/department/:id (PATCH)', () => {
+      const data = { upper: '1' };
+      return request(app.getHttpServer())
+        .patch('/department/10')
+        .send(data)
+        .expect(200);
+    });
+  });
+
+  describe('Air (e2e)', () => {
+    it('/air?sidoName=example (GET)', () => {
+      const sido = encodeURI('서울');
+      return request(app.getHttpServer())
+        .get(`/air?sidoName=${sido}`)
+        .expect(200);
+    });
+  });
+
+  afterAll((done) => {
+    app.close();
+    done();
+  });
 });
