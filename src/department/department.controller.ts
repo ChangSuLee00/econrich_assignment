7aa7ba708bedcea339e18f2422216f17a4a56d54
cslee <cslee0034@gmail.com>
2023-03-17 18:30:01 +0900

    feat: Add controller department findOne
    department findOne의 controller를 작성 했습니다.

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { DepartmentService } from './department.service';
+import { DepartmentFindOne } from './interfaces/findDept.type';
 
 @Controller('department')
 export class DepartmentController {
   constructor(private readonly departmentService: DepartmentService) {}
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.departmentService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<DepartmentFindOne> {
+    return this.departmentService.findOne(id);
   }
 }
